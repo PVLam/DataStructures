92377c15807456f9b4cb095d88db15b7695630ef
sv <phamphamhai99@gmail.com>
2018-09-13 09:56:44 +0700

    test

--- Linked List/mainSt.cpp
+++ Linked List/mainSt.cpp
@@ -6,14 +6,16 @@ using namespace std;
 int main()
 {
 	Stack<int> s;
+	s.Push(0);
+	s.Push(10);
+	s.Push(100);
+	cout<<s.Push()<<"\n";
+	cout<<s.Push()<<"\n";
+
 	Queue<int> q;
 	q.EnQueue(3);
 	q.EnQueue(5);
 	q.EnQueue(8);
 	cout<<q.DeQueue()<<"\n";
-	cout<<q.DeQueue()<<"\n";
-	cout<<q.DeQueue()<<"\n";
-	cout<<q.DeQueue()<<"\n";
-
 	return 0;
 }	
